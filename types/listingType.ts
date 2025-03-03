7d5c97844d38e63329047bd85b4e8917fa3e5e7e
nipunnishamaheeka <nishamaheeka123@gmail.com>
2025-03-03 07:29:42 +0530

    added images

--- types/listingType.ts
+++ types/listingType.ts
@@ -8,4 +8,9 @@ export interface ListingType{
     duration: string;
     location: string;
     category: string;
+    previewImage1: string;
+    previewImage2: string;
+    previewImage3: string;
+    previewImage4: string;
+
 }
